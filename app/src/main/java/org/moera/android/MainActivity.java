51fada5676887e97125c4d3a12aad6ed76fdf62a
Shmuel Melamud <smelamud@redhat.com>
2021-06-02 01:54:27 +0300

    Viewing web client in WebView without title bar.

--- app/src/main/java/org/moera/android/MainActivity.java
+++ app/src/main/java/org/moera/android/MainActivity.java
@@ -3,12 +3,19 @@ package org.moera.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String WEB_CLIENT_URL = "https://web.moera.org";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(WEB_CLIENT_URL);
     }
+
 }
