81b5303e64992676ddf2ceb49fefb2263b60cefa
Shmuel Melamud <smelamud@redhat.com>
2021-06-20 04:24:54 +0300

    Parsing push message content.

--- app/src/main/java/org/moera/android/PushEventHandler.java
+++ app/src/main/java/org/moera/android/PushEventHandler.java
@@ -2,8 +2,13 @@ package org.moera.android;
 
 import android.util.Log;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.MessageEvent;
+
+import org.moera.android.model.PushContent;
 
 public class PushEventHandler implements EventHandler {
 
@@ -25,8 +30,23 @@ public class PushEventHandler implements EventHandler {
 
     @Override
     public void onMessage(String event, MessageEvent messageEvent) {
-        Log.i(TAG, "Arrived event: " + event);
-        Log.i(TAG, "Data: " + messageEvent.getData());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        try {
+            PushContent pushContent = objectMapper.readValue(messageEvent.getData(),
+                    PushContent.class);
+            switch (pushContent.getType()) {
+                case STORY_ADDED:
+                    Log.i(TAG, "Story added: " + pushContent.getStory().getSummary());
+                    break;
+                case STORY_DELETED:
+                    Log.i(TAG, "Story deleted: " + pushContent.getId());
+                    break;
+            }
+        } catch (JsonProcessingException e) {
+            Log.e(TAG, "Error parsing push message: " + messageEvent.getData());
+            Log.e(TAG, "Exception:", e);
+        }
     }
 
     @Override
